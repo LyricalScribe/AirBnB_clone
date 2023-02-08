0101cf3202f207618fa492088fb4b0849a4827e7
ibukun <iadewaleodusanya@gmail.com>
2023-02-08 10:50:54 +0100

    task 3-4

---
+++ models/base_model.py
@@ -0,0 +1,44 @@
+import uuid
+from datetime import datetime, date
+"""The Base model which will be inherited across all class
+"""
+
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        """
+        The initialisation method that invokes on each call of the Basemodel
+        """
+        if kwargs:
+            f = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(kwargs[key], f)
+                if key != "__class__":
+                    setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
+
+    def __str__(self):
+        """Prints string format for our model"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Updates with new time"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary for our Class"""
+        my_dict = {}
+
+        my_dict["__class__"] = self.__class__.__name__
+        for k, v in self.__dict__.items():
+            if k == "created_at" or k == "updated_at":
+                my_dict[k] = v.isoformat() 
+            else:
+                my_dict[k] = v
+        return my_dict
