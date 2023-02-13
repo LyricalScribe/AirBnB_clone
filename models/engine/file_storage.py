39c4bb7582b8f51ee563a193f9f16795e4022555
ibukun <iadewaleodusanya@gmail.com>
2023-02-13 12:49:30 +0100

    yess

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -34,21 +34,9 @@ class FileStorage:
     def reload(self):
         from models.base_model import BaseModel
         from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
 
-        dct = {
-            'BaseModel': BaseModel,
-            'User': User,
-            "State": State,
-            "City": City,
-            "Amenity": Amenity,
-            "Place": Place,
-            "Review": Review,
-            }
+        dct = {'BaseModel': BaseModel, 'User': User}
+
 
         if os.path.exists(FileStorage.__file_path) is True:
             with open(FileStorage.__file_path, 'r') as f:
